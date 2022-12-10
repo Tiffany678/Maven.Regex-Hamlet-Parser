b525046641a6ec94e91958a156bb7df073dccee9
Tiffany678 <52tiffany.yang@gmail.comgit config --global user.name Tiffany678git config --global user.email 52tiffany.yang@gmail.com>
2022-12-10 00:23:57 -0500

    Submit

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,6 +10,25 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    Pattern p1 = Pattern.compile("Hamlet");
+    Pattern p2 = Pattern.compile("Horatio");
+
+    public Pattern getP1() {
+        return p1;
+    }
+
+    public Pattern getP2() {
+        return p2;
+    }
+    public boolean find(Pattern pattern){
+        return pattern.matcher(hamletData).find();
+    }
+    public String repalceStr(Pattern pattern, String matchString, String replaceString){
+        if(pattern.matcher(matchString).find()){
+          return  pattern.matcher(matchString).replaceAll(replaceString);
+        }
+        return null;
+    }
 
     public HamletParser(){
         this.hamletData = loadFile();
@@ -17,6 +38,7 @@ public class HamletParser {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
+
 
         try(Scanner scanner = new Scanner(file)){
             while(scanner.hasNextLine()){
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.*;
 
 public class HamletParserTest {
@@ -15,17 +18,23 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        hamletParser.repalceStr(hamletParser.getP2(), "Horatio", "Tariq");
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        hamletParser.repalceStr(hamletParser.getP2(), "Hamlet", "Leon");
+
     }
 
     @Test
     public void testFindHoratio() {
+        hamletParser.find(hamletParser.getP1());
     }
 
     @Test
     public void testFindHamlet() {
+        hamletParser.find(hamletParser.getP2());
+
     }
 }
